1fa735c78252ad17d199891dc9943730040a668b
AlekseyS93 <suslovwow@yandex.ru>
2024-03-20 19:25:22 +0300

    task04 done

--- person/InfantryBase.java
+++ person/InfantryBase.java
@@ -1,6 +1,5 @@
 package person;
 
-import behavior.ActionInterface;
 import behavior.CoordXY;
 
 import java.util.ArrayList;
@@ -31,10 +30,98 @@ public abstract class InfantryBase extends PersonBase {
         level = 1;
     }
 
-
-    @Override
-    public void step(ArrayList<PersonBase> enemies) {
-
+    /**
+     * Проверяет, не находится ли кто в заданных координатах
+     * @param pos     Позиция для проверки
+     * @param persons Список персонажей
+     * @return        true, если в заданной позиции никого нет.
+     */
+    private boolean isMoved(CoordXY pos, ArrayList<PersonBase> persons)
+    {
+        for (PersonBase p : persons)
+        {
+            if (p.position.equal(pos))
+                return false;
+        }
+        return true;
     }
 
+    private void move(PersonBase target, ArrayList<PersonBase> friends)
+    {
+        int[] px = {1, 0, -1, 0};       // координаты возможных ходов (вправо, вниз, влево, вверх)
+        int[] py = {0, 1, 0, -1};
+
+        // ищем кратчайший возможный ход в сторону противника
+        CoordXY newPos = new CoordXY(position.getX(),position.getY());
+        int minIdx = -1;
+        float minDist = Float.MAX_VALUE;
+        for (int i = 0; i < 4; i++)
+        {
+            newPos.setXY(position.getX()+px[i], position.getY()+py[i]);
+            if (isMoved(newPos, friends))
+            {
+                // сюда ходить можно, но нужно убедиться - кратчайший ли это путь?
+                float dist = position.fastDistance(target.position, px[i], py[i]);
+                if (dist < minDist)
+                {
+                    minIdx = i;
+                    minDist = dist;                }
+            }
+        }
+        if (minIdx == -1.0f)
+            return;
+
+        position.increment(px[minIdx], py[minIdx]);
+        System.out.println(name + ": перемещается на (" + position.getX() + ", " + position.getY() + ")");
+    }
+
+    private void attack(PersonBase target, boolean isMoved)
+    {
+        System.out.print(name + ": бьёт " + target);
+        int damage = getRound(power, 10) + (power / 10) * level;
+        boolean critical = (this.agility/3) >= rnd.nextInt(100);
+        if (critical)
+        {
+            damage *= 2.0f;
+        }
+        if (isMoved)
+            damage /= 2;                        // удар с хода
+
+        int res = target.getDamage(damage);
+        if (res > 0)
+        {
+            if (critical)
+                System.out.print(" и наносит критический удар в " + res + " повреждений!");
+            else
+                System.out.print(" и наносит " + res + " повреждений.");
+        } else {
+            System.out.print(", но " + target.name + " увернулся!");
+        }
+        if (target.health <= 0)
+        {
+            System.out.print("\n" + target + " вышел из чата!");
+        }
+    }
+
+    @Override
+    public void step(ArrayList<PersonBase> enemies, ArrayList<PersonBase> friends)
+    {
+        PersonBase target = this.findNearestPerson(enemies);
+        if (health <= 0 || target == null)
+            return;
+
+        if (position.distanceTo(target.position) < 1.5f)
+        {
+            // бьём
+            attack(target, false);
+        } else {
+            move(target, friends);
+            if (position.distanceTo(target.position) < 1.5f)
+            {
+                // бьём с ходу, с меньшей силой
+                attack(target, true);
+            }
+        }
+
+    }
 }
